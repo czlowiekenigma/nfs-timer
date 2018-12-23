c3520b4fe1045048b4de3e27e2a12a9cab500234
czlowiekenigma <46078707+czlowiekenigma@users.noreply.github.com>
2018-12-23 04:12:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,45 @@
 (function() {
-  let tick = 0;
+  let seconds = 3600;
   
   setInterval(function() {
-    if(tick == 0) {
-      $('h2').text('01:00:00');
-    } else if(tick == 1) {
-      $('h2').text('00:59:59');
-    } else if(tick == 2) {
-      $('h2').text('00:59:58');
-    } else if(tick == 3) {
-      $('h2').text('00:59:57');
-    } else if(tick == 4) {
-      $('h2').text('00:59:56');
-    } else if(tick == 5) {
-      $('h2').text('00:59:55');
+    seconds--;
+    if(seconds == 3594) {
+      seconds = 3600;
     }
     
-    tick++;
-    if(tick == 6) {
-      tick = 0; 
+    let h = Math.floor(seconds / 3600);
+    let m = Math.floor((seconds % 3600) / 60);
+    let s = Math.floor((seconds % 3600) % 60);
+    
+    if(h < 10) {
+      h = `0${h}`; 
     }
+    if(m < 10) {
+      m = `0${m}`; 
+    }
+    if(s < 10) {
+      s = `0${s}`; 
+    }
+    
+    $('h2').text(`${h}:${m}:${s}`);
+    
+//     if(tick == 0) {
+//       $('h2').text('01:00:00');
+//     } else if(tick == 1) {
+//       $('h2').text('00:59:59');
+//     } else if(tick == 2) {
+//       $('h2').text('00:59:58');
+//     } else if(tick == 3) {
+//       $('h2').text('00:59:57');
+//     } else if(tick == 4) {
+//       $('h2').text('00:59:56');
+//     } else if(tick == 5) {
+//       $('h2').text('00:59:55');
+//     }
+    
+//     tick++;
+//     if(tick == 6) {
+//       tick = 0; 
+//     }
   }, 1000);
 })();
